bd611822f7b677774c4fe196b65715a1080006d9
Gulomjon <gulomjon.malikov1996@mail.ru>
2020-08-24 19:55:55 +0500

    First Commit

---
+++ Javascript/variables.js
@@ -0,0 +1,13 @@
+let listening = 6.5;
+let reading = 7;
+let writing = 5.5;
+let speaking = 7.5;
+
+let sum = (listening + reading + writing + speaking)/4
+
+console.log(sum);
+
+let firstName = "Gulomjon";
+let lastName = "Malikov";
+
+console.log(firstName + ' ' + lastName);
